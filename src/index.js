487e696ec07205469f6c0ba1c14cc096c34c862b
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-02-23 17:50:51 +0300

    new approach to this task

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  var BracketStr=str;
-  var SetBrck=[];
-//section set of bracketconfig
-  for(var i=0; i<bracketsConfig.length; i++ ){
-  var a='';
-      for(var j=0; j<2; j++ ){
-      a+=bracketsConfig[i][j];
-      }
-      SetBrck[i]=a;
+    let compareStr;
+    let ConfigVariants=bracketsConfig.length;
+    while(str.length>0&&compareStr!==str){
+        compareStr=str;
+    for(let i=0; i<ConfigVariants; i++){
+        str=str.replace(bracketsConfig[i].join().replace(",",""),"");
     }
-   // works ---> var a= BracketStr.includes(SetBrck[2]);
-  do{
-      // CndChckVar update section
-
-   var CndChckVar=false;
-       for( var i=0; i<SetBrck.length; i++){
-           if(BracketStr.includes(SetBrck[i])){
-               CndChckVar=true;
-               break;
-           }
-       }
-        //  BracketStr update esction
-       for( var i=0; i<SetBrck.length; i++){
-       BracketStr=BracketStr.replace(SetBrck[i],"");
-        }
-   }
-   while(CndChckVar);
-
-// section closing parenthesis check
-/* for( var i=0; i<SetBrck.length; i++){
-while(BracketStr.includes(SetBrck[i])){
-    BracketStr.search(SetBrck[i])
-    BracketStr-=SetBrck[i];
-}
-
- }*/
-
-  return Boolean(!BracketStr) ;
+    }
+   return Boolean(!str);
 }
